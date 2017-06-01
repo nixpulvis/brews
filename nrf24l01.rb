9df78751ba570accc232ef53d575397540a4b37e
Nathan Lilienthal <nathan@nixpulvis.com>
2017-06-01 14:02:02 -0700

    Oops, it's not version 0.0.3.

--- nrf24l01.rb
+++ nrf24l01.rb
@@ -2,7 +2,7 @@ class Nrf24l01 < Formula
     desc "AVR library for the Nordic nRF24L01."
     homepage "https://github.com/nixpulvis/nrf24l01"
     url "https://github.com/nixpulvis/nrf24l01/archive/0.0.4.tar.gz"
-    version "0.0.3"
+    version "0.0.4"
     sha256 "cc7458ecaa8a5c14d45cadfd95bc69e8fd8f17a1b04a252ce90ed362ae36dc25"
 
     depends_on 'nixpulvis/formulas/avrm'
